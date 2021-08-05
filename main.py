ed4d8eb64954e33671a136802d306c41a067ce15
Vidit Bhatwadekar <viditbhatwadekar@berkeley.edu>
2021-08-05 15:13:07 +0800

    added okbr_url images

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 # from chatterbot import ChatBot
 # from chatterbot.trainers import ListTrainer
 import streamlit as st
+from bottle import route, template, run
+import praw
 
 
 
@@ -10,25 +12,35 @@ OKBR_df = pd.read_csv('OKBR.csv')
 OKBR_arr = OKBR_df.iloc[:, 1].to_numpy()
 OKBR_ls = OKBR_arr.tolist()
 
-st.sidebar.title("NLP Bot")
-st.title("okbuddyretard Bot")
+OKBR_df = OKBR_df.rename(columns={'Unnamed: 0':'Number', '0': 'Quote'})
+
+st.title("r/okbr MEme funtime")
+
+st.markdown("""
+### okbudy time for todays mem
+
+
+""")
+
+
+
+
+
 
 # bot = ChatBot('Yahoo')
 
-ind = 1
-if st.sidebar.button('Initialize chatbot'):
-    # trainer = ListTrainer(bot)
-    # trainer.train(OKBR_ls)
-    st.title("Your bot is ready to talk to you")
-    ind = ind + 1
+# ind = 1
+# if st.sidebar.button('Initialize chatbot'):
+#     # trainer = ListTrainer(bot)
+#     # trainer.train(OKBR_ls)
+#     st.title("Your bot is ready to talk to you")
+#     ind = ind + 1
 
-def get_text():
-    input_text = st.text_input("You: ","So, what's in your mind")
-    return input_text
-
-user_input = get_text()
+# def get_text():
+#     input_text = st.text_input("You: ","So, what's in your mind")
+#     return input_text
+#
+# user_input = get_text()
 
 if True:
-    st.text_area("Bot:", value=OKBR_df.sample()[0], height=200, max_chars=None, key=None)
-else:
-    st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
+    st.markdown(OKBR_df.sample()['Quote'].iloc[0])
