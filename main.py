592b9496009e09a691333924112ffd288e9c9d83
Vidit Bhatwadekar <viditbhatwadekar@berkeley.edu>
2021-08-05 13:22:46 +0800

    Got rid of chatterbot just made random quote generator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ListTrainer
 import streamlit as st
 
 
@@ -13,12 +13,12 @@ OKBR_ls = OKBR_arr.tolist()
 st.sidebar.title("NLP Bot")
 st.title("okbuddyretard Bot")
 
-bot = ChatBot('Yahoo')
+# bot = ChatBot('Yahoo')
 
 ind = 1
 if st.sidebar.button('Initialize chatbot'):
-    trainer = ListTrainer(bot)
-    trainer.train(OKBR_ls)
+    # trainer = ListTrainer(bot)
+    # trainer.train(OKBR_ls)
     st.title("Your bot is ready to talk to you")
     ind = ind + 1
 
@@ -29,6 +29,6 @@ def get_text():
 user_input = get_text()
 
 if True:
-    st.text_area("Bot:", value=bot.get_response(user_input), height=200, max_chars=None, key=None)
+    st.text_area("Bot:", value=OKBR_df.sample()[0], height=200, max_chars=None, key=None)
 else:
     st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
