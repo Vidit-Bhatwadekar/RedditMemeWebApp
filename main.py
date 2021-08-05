93c3fd516bc30d05d246c0917e2e668c244bc4a7
Vidit Bhatwadekar <viditbhatwadekar@berkeley.edu>
2021-08-05 13:02:09 +0800

    readded Chatterbot and changed reqs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 import streamlit as st
 
 
@@ -13,22 +13,22 @@ OKBR_ls = OKBR_arr.tolist()
 st.sidebar.title("NLP Bot")
 st.title("okbuddyretard Bot")
 
-# bot = ChatBot('Yahoo')
-#
-# ind = 1
-# if st.sidebar.button('Initialize chatbot'):
-#     trainer = ListTrainer(bot)
-#     trainer.train(OKBR_ls)
-#     st.title("Your bot is ready to talk to you")
-#     ind = ind + 1
-#
-# def get_text():
-#     input_text = st.text_input("You: ","So, what's in your mind")
-#     return input_text
-#
-# user_input = get_text()
-#
-# if True:
-#     st.text_area("Bot:", value=bot.get_response(user_input), height=200, max_chars=None, key=None)
-# else:
-#     st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
+bot = ChatBot('Yahoo')
+
+ind = 1
+if st.sidebar.button('Initialize chatbot'):
+    trainer = ListTrainer(bot)
+    trainer.train(OKBR_ls)
+    st.title("Your bot is ready to talk to you")
+    ind = ind + 1
+
+def get_text():
+    input_text = st.text_input("You: ","So, what's in your mind")
+    return input_text
+
+user_input = get_text()
+
+if True:
+    st.text_area("Bot:", value=bot.get_response(user_input), height=200, max_chars=None, key=None)
+else:
+    st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
